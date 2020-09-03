f1f39efdda71c1f3972ed0761674a1dc337dc2b9
John Alonso <jvalon9455@gmail.com>
2020-09-03 13:06:51 -0400

    added proompt for password length

--- script.js
+++ script.js
@@ -25,12 +25,16 @@ var generateBtn = document.querySelector("#generate");
 
 var lowerString = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperString = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberString = "0123456789";
+var numberString = ["0","1","2","3","4","5","6","7","8","9"];
 var symbolString = "~`!@#$%^&*()_+-=[]{};:',.<>/?";
 
 // Prompt user for password length
 var pwLength = prompt("Password must be 8-128 characters long. How long would you like yours to be?");
 
+if (pwLength > 8 && pwLength < 128){
+  ("Password length:" + pwLength);
+}
+  
 
 
 // Write password to the #password input
