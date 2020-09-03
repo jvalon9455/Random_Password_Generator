776c02abcb615adab316aa4310a762f3c06814b7
John Alonso <jvalon9455@gmail.com>
2020-09-03 17:23:37 -0400

    built string for user preference

--- script.js
+++ script.js
@@ -24,35 +24,66 @@ var generateBtn = document.querySelector("#generate");
 // Prompt user for password length
 var pwLength = prompt("Password must be 8-128 characters long. How long would you like yours to be?");
 
-if (pwLength > 8 && pwLength < 128){
+if (pwLength >= 8 && pwLength < 128){
   ("Password length:" + pwLength);
 }
 else{
   alert("Please enter valid character length.");
   prompt("Password must be 8-128 characters long. How long would you like yours to be?");
 }
+
 //  Ask user if they want lowercase, uppercase, numbers, and/or symbols in their password
-var hasLowercase = confirm("Add lowercase letters to password?");
-var hasUppercase = confirm("Add uppercase letters to password?");
-var hasNumbers = confirm("Add numbers to password?");
-var hasSymbols = confirm("Add symbols to password?");
+
+var addLowercase = confirm("Add lowercase letters to password?");
+// console.log("Use lowercase:" + lowerCaseCharacters);
+var addUppercase = confirm("Add uppercase letters to password?");
+// console.log("Use uppercase:" + upperCaseCharacters);
+var addNumbers = confirm("Add numbers to password?");
+// console.log("Use numbers:" + numberCharacters)
+var addSymbols = confirm("Add symbols to password?");
+// console.log("Use symbols:" + symbolCharacters);
 
 // Added four sets of different character strings to generate password
 
-var lowerString = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperString = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberString = ["0","1","2","3","4","5","6","7","8","9"];
-var symbolString = ["~`!@#$%^&*()_+-=[]{};:',.<>/?"];  
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+var symbolCharacters = ["~`!@#$%^&*()_+-=[]{};:',.<>/?"];  
 
 if (
-  hasLowercase === false &&
-  hasUppercase=== false &&
-  hasNumbers === false &&
-  hasSymbols === false 
+  addLowercase === false &&
+  addUppercase=== false &&
+  addNumbers === false &&
+  addSymbols === false 
 ) {
   alert("Please select one character type to generate password.");
-  
 }
+
+// Build string of characters based on users preference
+
+var chosenCharacters = "";
+if (addLowercase === true){
+  chosenCharacters = chosenCharacters.concat(lowerCaseCharacters);
+}
+if (addUppercase === true){
+  chosenCharacters = chosenCharacters.concat(upperCaseCharacters);
+}
+if (addNumbers === true){
+  chosenCharacters = chosenCharacters.concat(numberCharacters);
+}
+if (addSymbols === true){
+  chosenCharacters = chosenCharacters.concat(symbolCharacters);
+}
+// Checked to see if it shows in console. Check.
+console.log(chosenCharacters)
+
+// Create empty string to hold random password
+var randomPassword = "";
+
+// for (var i = 0; i < pwLength; i++){
+//   Math.floor(Math.random()*
+
+
 
 
 // Write password to the #password input
