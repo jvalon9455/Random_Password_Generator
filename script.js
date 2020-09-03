e4e44aabafec7ddeab72172ab05b0c934c463927
John Alonso <jvalon9455@gmail.com>
2020-09-03 09:57:26 -0400

    added prompt for user to choose paswword length

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+// ## Acceptance Criteria
+
+// ```
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// ```
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Added four sets of different character strings to generate password
 
-var lowerString = "abcdefghijklmnopqrstuvwxyz";
-var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerString = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperString = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberString = "0123456789";
 var symbolString = "~`!@#$%^&*()_+-=[]{};:',.<>/?";
+
+// Prompt user for password length
+var pwLength = prompt("Password must be 8-128 characters long. How long would you like yours to be?");
+
+
 
 // Write password to the #password input
 function writePassword() {
