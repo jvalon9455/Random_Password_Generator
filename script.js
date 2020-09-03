8267820970bf341757c69f40e21ba3b94d5dee8c
John Alonso <jvalon9455@gmail.com>
2020-09-03 09:35:17 -0400

    created 4 strings of different characters to generate password

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Added four sets of different character strings to generate password
+
+var lowerString = "abcdefghijklmnopqrstuvwxyz";
+var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberString = "0123456789";
+var symbolString = "~`!@#$%^&*()_+-=[]{};:',.<>/?";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
