44a235a236c8ffbb30d907714657ea385e991488
John Alonso <jvalon9455@gmail.com>
2020-09-03 22:39:54 -0400

    edited array characters to get rid of commas

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ var generateBtn = document.querySelector("#generate");
 var chosenCharacters = "";
 
 // Added four sets of different character strings to generate password
-var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseCharacters = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCaseCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numberCharacters = ["1234567890"];
 var symbolCharacters = ["~`!@#$%^&*()_+-=[]{};:',.<>/?"];
 
 function generatePassword() {
